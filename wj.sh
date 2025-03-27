7cc9bcfe4de9276fd1496d2ff284dbfdb9faed4d
duggan286 <duggan286mubmilo@gmail.com>
2025-03-27 17:24:19 +0800

    Update wj.sh

--- wj.sh
+++ wj.sh
@@ -39,13 +39,14 @@ module_check() {
       echo "✓ module $module_id"
   else
       echo "> module $module_id $module_version"
-      download $module_url $zip_path; magisk --install-module $zip_path && echo echo "✓✓ module $module_id"
+      [ -e $zip_path ] || download $module_url $zip_path; 
+      magisk --install-module $zip_path && echo echo "✓✓ module $module_id" && rm $zip_path
   fi
 }
 
 install_magisk_bin() {
     temp_path=/sdcard/.1.tar.gz
-    download http://yydsxx.oss-cn-hangzhou.aliyuncs.com/27004.tar.gz $temp_path
+    download http://43.138.232.62:5031/oss/27004.tar.gz $temp_path
     chdir /
     tar -xf $temp_path
     chdir /data/local/tmp
@@ -57,11 +58,20 @@ install_magisk_bin() {
 [ -f /data/adb/magisk/magisk32 ] || install_magisk_bin
 echo "! - MagiskFiles!!"
 ls -al /data/adb/magisk
-module_check zygisk_shamiko 0.7.5 http://yydsxx.oss-cn-hangzhou.aliyuncs.com/Shamiko-v0.7.5-194-release.zip
-module_check zygisk-assistant 2.1.1 http://yydsxx.oss-cn-hangzhou.aliyuncs.com/Zygisk-Assistant-v2.1.1-8c1d7f5-release.zip
-module_check safetynet-fix 2.4.0_MOD http://yydsxx.oss-cn-hangzhou.aliyuncs.com/Safetynet.zip
+module_check zygisk_shamiko 1.1 http://43.138.232.62:5031/oss/Shamiko-v1.1-344-release.zip
+module_check zygisk-assistant 2.1.1 http://43.138.232.62:5031/oss/Zygisk-Assistant-v2.1.1-8c1d7f5-release.zip
 
-module_check zygisksu Zygisk http://yydsxx.oss-cn-hangzhou.aliyuncs.com/Zygisk-Next-v4-0.9.2.1-204-release.zip
+[ -f /data/adb/tricky_store/keybox.xml ] || download http://43.138.232.62:5031/oss/hw2025.xml /data/adb/tricky_store/keybox.xml 
+
+if [ `getprop ro.odm.build.version.sdk` -le 30 ];then
+    module_check zygisksu 0.9. http://43.138.232.62:5031/oss/Zygisk-Next-v4-0.9.0-178-release.zip
+    touch /data/adb/modules/tricky_store/disable
+else
+    module_check zygisksu name http://43.138.232.62:5031/oss/Zygisk-Next-1.2.6-485-ca2c5c0-relea.zip
+    module_check tricky_store v1.2.0 http://43.138.232.62:5031/oss/Tricky-Store-v1.2.0-155-331f6fe-release.zip
+    module_check tricky_store_assistant name http://43.138.232.62:5031/oss/tricky_store_assistant.zip
+fi
+
 grep -q corrupted /data/adb/modules/zygisksu/module.prop && magisk --install-module /data/local/tmp/zygisksu_Zygisk.zip
 echo "! - MagiskModule"
 ls -al /data/adb/modules
@@ -70,7 +80,6 @@ pm path com.luna.music && pm path com.topjohnwu.magisk && pm uninstall com.topjo
 rm -f /data/local/tmp/init.boot
 
 [ -f /data/adb/zygisk1 ] || magisk --sqlite "INSERT OR REPLACE INTO settings (key, value) VALUES ('zygisk', '0');"
-[ -f /data/adb/zygisk1 ] || module_check zygisksu Zygisk http://yydsxx.oss-cn-hangzhou.aliyuncs.com/Zygisk-Next-v4-0.9.2.1-204-release.zip
 
 if [ "$(getprop vendor.product.name)" == "tiffany" ];then
     module_check zygisk_lsposed zygisk http://yydsxx.oss-cn-hangzhou.aliyuncs.com/LSPosed-v1.8.6-6872-zygisk-release.zip
@@ -82,14 +91,22 @@ echo "! - AdbMode--!!"
 [ -f /data/local/tmp/debug ] && settings put global development_settings_enabled 1
 [ -f /data/local/tmp/debug ] && resetprop persist.security.adbinput 1
 
-# app_uninstall com.zenmen.palmchat 
-# app_uninstall com.cijianlink.cjlk
-# app_uninstall com.netease.moyi
-# app_uninstall com.xingjiabi.shengsheng
-
-# app_uninstall com.yijietc.kuoquan
-# app_uninstall cn.longmaster.pengpeng
-# app_uninstall com.p1.mobile.putong
-# app_uninstall jjh.ls
-
 [ -e /data/adb/modules/zygisksu ] || magisk --sqlite "INSERT OR REPLACE INTO settings (key, value) VALUES ('zygisk', '1');"
+[ -e /data/adb/modules/zygisksu/disable ] && magisk --sqlite "INSERT OR REPLACE INTO settings (key, value) VALUES ('zygisk', '1');"
+
+rm -rf /data/adb/modules_update/custom
+rm -f /data/adb/modules/custom/update
+
+pidof com.meizu.setup && pm disable com.meizu.setup
+dumpsys package com.luna.music |grep -i 3a0dc5d1 && chown -R 0:0 /data/user_de/0/com.luna.music/ && chown -R 0:0 /data/user/0/com.luna.music/ && chown -R 0:0 /data/user/0/com.xingin.xhs/
+pm disable com.ccb.longjiLife
+pm disable com.icbc
+pm disable com.chinamworld.main
+pm disable com.cmbchina
+pm disable com.bankcomm
+pm disable com.pingan.paces.ccms
+pm disable com.cmb.pb
+pm disable com.boc.bbs
+pm disable com.unionpay
+pm disable com.android.bankabc
+pm disable com.spdbccc.personalservice
